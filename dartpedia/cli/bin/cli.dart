5f8869d60673fde5f58808e748fb87fa7e2514d5
JessicaKndsvtr <jessica-kindsvater@web.de>
2026-02-09 00:31:01 +0100

    new project dartpedia - initial commit

---
+++ dartpedia/cli/bin/cli.dart
@@ -0,0 +1,15 @@
+const version = '0.0.1';
+
+void main(List<String> arguments) {
+  if(arguments.isEmpty || arguments.first == 'help') {
+    printUsage();
+  } else if(arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  } else {
+    printUsage();
+  }
+}
+
+void printUsage() {
+  print("The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'");
+}
