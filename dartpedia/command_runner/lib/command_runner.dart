d6a45c2474c5f5a3c13a4cd1b23a2bca201b8ebb
JessicaKndsvtr <jessica-kindsvater@web.de>
2026-04-09 21:04:47 +0200

    Add: new package for command-line handling

---
+++ dartpedia/command_runner/lib/command_runner.dart
@@ -0,0 +1,8 @@
+/// A simple command runner to handle command_line arguments. 
+///
+/// More dartdocs go here.
+library;
+
+export 'src/command_runner_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
---
+++ dartpedia/command_runner/lib/src/command_runner_base.dart
@@ -0,0 +1,8 @@
+// public command-line functions for dartpedia
+
+class CommandRunner {
+  Future<void> run(List<String> input) async {
+    print('CommandRunner received arguments: $input');
+  }
+}
+//TODO: https://dart.dev/learn/tutorial/packages-libs task 3
